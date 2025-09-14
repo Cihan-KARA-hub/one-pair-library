5411c733597989ad32c31b99204891dad2acf3ef
Cihan Kara <cihankara632@gmail.com>
2025-09-14 18:09:34 +0300

    Rules init

--- src/main/java/com/pairone/library/rules/BookRule.java
+++ src/main/java/com/pairone/library/rules/BookRule.java
@@ -1,2 +1,20 @@
-package com.pairone.library.rules;public class BookRule {
+package com.pairone.library.rules;
+
+import com.pairone.library.entity.Book;
+import com.pairone.library.repository.BookRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class BookRule {
+    private final BookRepository bookRepository;
+
+    public BookRule(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book findById(int id) {
+        return bookRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "book not found "));
+    }
 }
